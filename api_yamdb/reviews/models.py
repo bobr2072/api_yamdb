14cf46f18468bc7423c065e135b91b52518147b6
Vladislav Pyatov <vladpyatov@yandex.ru>
2022-08-17 13:33:20 +0300

    Add review and comment models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,74 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Review(models.Model):
+    """Review model"""
+    RATING_CHOICES = [
+        (1, 1), (2, 2), (3, 3), (4, 4), (5, 5),
+        (6, 6), (7, 7), (8, 8), (9, 9), (10, 10)
+    ]
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Произведение',
+    )
+    text: models.TextField = models.TextField(
+        verbose_name='Текст',
+        help_text='Текст отзыва'
+    )
+    author: models.ForeignKey = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Автор отзыва',
+    )
+    score: models.PositiveSmallIntegerField = models.PositiveSmallIntegerField(
+        choices=RATING_CHOICES,
+        verbose_name='Рейтинг',
+        help_text='Рейтинг произведения'
+    )
+    pub_date: models.DateTimeField = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации',
+        help_text='Дата публикации отзыва'
+    )
+
+    class Meta:
+        """Review metaclass"""
+        constraints = [
+            models.UniqueConstraint(
+                fields=('title', 'author'),
+                name="unique_title_author_pair"
+            )
+        ]
+
+    def __str__(self):
+        return self.text[:15]
+
+
+class Comment(models.Model):
+    """Comment model"""
+    review: models.ForeignKey = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Комментарий',
+    )
+    text: models.TextField = models.TextField(
+        verbose_name='Текст',
+        help_text='Текст комментария'
+    )
+    author: models.ForeignKey = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Автор комментария',
+    )
+    pub_date: models.DateTimeField = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации',
+        help_text='Дата публикации комментария'
+    )
