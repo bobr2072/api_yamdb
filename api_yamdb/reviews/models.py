33fc9c2870f373d9051c2dd634ca8ca16d531ef2
Vladislav Pyatov <vladpyatov@yandex.ru>
2022-08-23 23:34:43 +0300

    add management command

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.core.validators import MaxValueValidator
 from django.utils import timezone
 
-from users.models import User
+User = get_user_model()
 
 
 class Category(models.Model):
